9ddf11f37701ac906f14eefb3819de5716de7fa0
Novi Panna Vira <49115024+pannavr@users.noreply.github.com>
2020-10-20 00:28:10 +0700

    mail dan fix icon

--- consumeApiFinal/src/main/java/com/metrodata/consumeApiFinal/services/EmailNotificationService.java
+++ consumeApiFinal/src/main/java/com/metrodata/consumeApiFinal/services/EmailNotificationService.java
@@ -5,6 +5,7 @@
  */
 package com.metrodata.consumeApiFinal.services;
 
+import com.metrodata.consumeApiFinal.entities.ScheduleTest;
 import com.metrodata.consumeApiFinal.entities.dao.RegisterInput;
 import java.nio.charset.StandardCharsets;
 import javax.mail.MessagingException;
@@ -47,6 +48,76 @@ public class EmailNotificationService {
         helper.setTo(input.getEmail());
         helper.setText(html, true);
         helper.setSubject("Register Success - Metrodata Recruitment");
+        helper.setFrom("metrodata.recruitment3@gmail.com");
+
+        javaMailSender.send(message);
+    }
+    public void sendPsikotest(ScheduleTest input) throws MessagingException {
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+
+        Context context = new Context();
+        context.setVariable("Date", input.getDate());
+        context.setVariable("startTime", input.getStartTime());
+        context.setVariable("endTime", input.getEndTime());
+        context.setVariable("test", input.getTest().getName());
+
+        String html = templateEngine.process("template-psikotest", context);
+
+        System.out.println(input.getApply());
+        helper.setTo(input.getApply().getCandidate().getEmail());
+        helper.setText(html, true);
+        helper.setSubject("Schedule Psikotest Test - Metrodata Recruitment");
+        helper.setFrom("metrodata.recruitment3@gmail.com");
+
+        javaMailSender.send(message);
+    }
+    public void sendTechnical(ScheduleTest input) throws MessagingException {
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+
+        Context context = new Context();
+        context.setVariable("Date", input.getDate());
+        context.setVariable("startTime", input.getStartTime());
+        context.setVariable("endTime", input.getEndTime());
+        context.setVariable("test", input.getTest().getName());
+
+        String html = templateEngine.process("template-technical", context);
+
+        System.out.println(input.getApply());
+        helper.setTo(input.getApply().getCandidate().getEmail());
+        helper.setText(html, true);
+        helper.setSubject("Schedule Technical Test - Metrodata Recruitment");
+        helper.setFrom("metrodata.recruitment3@gmail.com");
+
+        javaMailSender.send(message);
+    }
+    public void sendInterview(ScheduleTest input) throws MessagingException {
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+
+        Context context = new Context();
+        context.setVariable("Name", input.getApply().getCandidate().getFullName());
+        context.setVariable("Date", input.getDate());
+        context.setVariable("startTime", input.getStartTime());
+        context.setVariable("endTime", input.getEndTime());
+        context.setVariable("test", input.getTest().getName());
+
+        String html = templateEngine.process("template-interview", context);
+
+        System.out.println(input.getApply());
+        helper.setTo(input.getApply().getCandidate().getEmail());
+        helper.setText(html, true);
+        helper.setSubject("Schedule Interview Test - Metrodata Recruitment");
         helper.setFrom("metrodata.recruitment3@gmail.com");
 
         javaMailSender.send(message);
