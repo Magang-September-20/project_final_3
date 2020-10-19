87fed23e4014cd55dba775d4c453f53de899d1b0
Novi Panna Vira <49115024+pannavr@users.noreply.github.com>
2020-10-20 02:29:08 +0700

    dataTable

--- consumeApiFinal/src/main/java/com/metrodata/consumeApiFinal/repositories/ScheduleTestRepository.java
+++ consumeApiFinal/src/main/java/com/metrodata/consumeApiFinal/repositories/ScheduleTestRepository.java
@@ -18,23 +18,21 @@ import org.springframework.stereotype.Repository;
  * @author pannavr
  */
 @Repository
-public interface ScheduleTestRepository extends JpaRepository<ScheduleTest, Integer>{
+public interface ScheduleTestRepository extends JpaRepository<ScheduleTest, Integer> {
 
 //    public void save(ScheduleTestInput scheduleTestInput);
-
-    @Query(value="SELECT MAX(id) FROM tb_tr_schedule_test",nativeQuery = true)
+    @Query(value = "SELECT MAX(id) FROM tb_tr_schedule_test", nativeQuery = true)
     public int getMaxId();
 //    public void save(Map<String, Object> registerApi);
 
 //    public String save(ScheduleRepository scheduleRepository);
-    
-       @Query(value = "SELECT * FROM  tb_tr_schedule_test join tb_tr_program_apply  on tb_tr_program_apply.id =tb_tr_schedule_test.apply join tb_tr_result on tb_tr_result.id = tb_tr_schedule_test.id join tb_m_test on tb_tr_schedule_test.test = tb_m_test.id WHERE tb_m_test.id = 1 and tb_tr_result.is_passed = 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM  tb_tr_schedule_test join tb_tr_program_apply  on tb_tr_program_apply.id =tb_tr_schedule_test.apply join tb_tr_result on tb_tr_result.id = tb_tr_schedule_test.id join tb_m_test on tb_tr_schedule_test.test = tb_m_test.id WHERE tb_m_test.id = 1 and tb_tr_result.is_passed = 1", nativeQuery = true)
     public List<ScheduleTest> ShowPsikotes();
-    
+
     @Query(value = "SELECT * FROM  tb_tr_schedule_test join tb_tr_program_apply  on tb_tr_program_apply.id =tb_tr_schedule_test.apply join tb_tr_result on tb_tr_result.id = tb_tr_schedule_test.id join tb_m_test on tb_tr_schedule_test.test = tb_m_test.id WHERE tb_m_test.id = 2 and tb_tr_result.is_passed = 1", nativeQuery = true)
     public List<ScheduleTest> ShowTechnical();
-    
+
     @Query(value = "SELECT * FROM  tb_tr_schedule_test join tb_tr_program_apply  on tb_tr_program_apply.id =tb_tr_schedule_test.apply join tb_tr_result on tb_tr_result.id = tb_tr_schedule_test.id join tb_m_test on tb_tr_schedule_test.test = tb_m_test.id WHERE tb_m_test.id = 3 and tb_tr_result.is_passed = 1", nativeQuery = true)
     public List<ScheduleTest> ShowInterview();
-    
+
 }
