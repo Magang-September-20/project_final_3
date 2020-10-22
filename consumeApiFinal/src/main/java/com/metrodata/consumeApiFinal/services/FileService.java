ee2f23f834531d65543d1cd73d8f7f1829deeb07
Jonathan purnama halim <46183646+nattnathan@users.noreply.github.com>
2020-10-23 05:33:15 +0700

    fix overwrite upload

--- consumeApiFinal/src/main/java/com/metrodata/consumeApiFinal/services/FileService.java
+++ consumeApiFinal/src/main/java/com/metrodata/consumeApiFinal/services/FileService.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -61,15 +62,13 @@ public class FileService {
                 try {
                     Files.createDirectory(uploadPath);
                 } catch (IOException ex) {
-                    Logger.getLogger(RegisterService.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
 
             try {
                 Path filePath = uploadPath.resolve(filename);
-                Files.copy(file.getInputStream(), filePath);
+                Files.copy(file.getInputStream(), filePath,StandardCopyOption.REPLACE_EXISTING);
             } catch (IOException ex) {
-                Logger.getLogger(RegisterService.class.getName()).log(Level.SEVERE, null, ex);
             }
         } else {
             filename = "";
