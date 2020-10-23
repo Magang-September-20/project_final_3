48fdfd9fe47c0fc07c6dbeaa74708e9c6cafd8a1
Olivia Michelle <71066168+Olivliv@users.noreply.github.com>
2020-10-23 11:58:24 +0700

    nath

--- consumeApiFinal/src/main/java/com/metrodata/consumeApiFinal/services/EmailNotificationProgram.java
+++ consumeApiFinal/src/main/java/com/metrodata/consumeApiFinal/services/EmailNotificationProgram.java
@@ -33,7 +33,6 @@ public class EmailNotificationProgram {
     }
     @Autowired
     private SpringTemplateEngine templateEngine;
-    
 
     public void sendEmailCandidate(User candidate, User hr, ProgramApply pa) throws MessagingException {
         MimeMessage message = javaMailSender.createMimeMessage();
@@ -47,16 +46,32 @@ public class EmailNotificationProgram {
 
 //        System.out.println("ini program " + pa.getProgram().getName());
 //        System.out.println("ini note " + pa.getNote());
-
 //        System.out.println(pa.getCandidate().getEmail());
 //        System.out.println(pa.getCandidate().getFullName());
 //        System.out.println(pa.getHr().getFullName());
 //        helper.setTo(candidate.getEmail()); //ini di isi pa.getCandidate().getEmail()
         helper.setText(html, true);
-        helper.setSubject("Register Program Success - Metrodata Recruitment "+pa.getProgram().getName());
+        helper.setSubject("Register Program Success - Metrodata Recruitment " + pa.getProgram().getName());
         System.out.println(pa.getProgram().getName());
         helper.setFrom("metrodata.recruitment3@gmail.com");
         javaMailSender.send(message);
     }
-    
+
+    public void sendEmailCongrat(User candidate, ProgramApply pa) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
+        Context context = new Context();
+        context.setVariable("name", candidate.getFullName());
+        context.setVariable("program", pa.getProgram().getName());
+        context.setVariable("email", candidate.getEmail());
+        String html = templateEngine.process("template-congrats", context);
+        helper.setTo("michelleolivia68@gmail.com"); //ini di isi pa.getCandidate().getEmail()
+        helper.setText(html, true);
+        helper.setSubject("Congratulation : You are accepted " + pa.getProgram().getName());
+        System.out.println(pa.getProgram().getName());
+        helper.setFrom("metrodata.recruitment3@gmail.com");
+        javaMailSender.send(message);
+    }
+
 }
